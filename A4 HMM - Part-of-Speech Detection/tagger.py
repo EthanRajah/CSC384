a47e9667bdd47eccb0c56f9494bb6176d98f75df
Ethan Rajah <ethan.rajah@outlook.com>
2023-03-30 00:57:29 -0400

    Text file parser

--- A4 HMM - Part-of-Speech Detection/tagger.py
+++ A4 HMM - Part-of-Speech Detection/tagger.py
@@ -6,7 +6,24 @@ from collections import Counter
 class HMM:
     '''Hidden Markov Model class to store relevent matrices and sequences'''
     def __init__(self):
-        pass
+        observations = []
+        hiddenStates = []
+
+def parseTraining(hmm, training_list):
+    '''Parse training data and create two lists, one for the words (observations) and one for the POS tags (hidden states)'''
+    words = []
+    posTags = []
+
+    for training_file in training_list:
+        # Open training file, for each line split on colon, appending first element to words list and second element to posTags list
+        with open(training_file, 'r') as f:
+            for line in f:
+                line = line.split(':')
+                words.append(line[0].strip())
+                posTags.append(line[1].strip())
+    
+    hmm.observations = words
+    hmm.hiddenStates = posTags
 
 def sentenceSeperation():
     '''Seperate input text file by text symbol of choice (i.e. periods, colons etc)'''
@@ -45,23 +62,19 @@ if __name__ == '__main__':
     parser.add_argument(
         "--testfile",
         type=str,
-        required=True,
+        required=False,
         help="One test file."
     )
     parser.add_argument(
         "--outputfile",
         type=str,
-        required=True,
+        required=False,
         help="The output file."
     )
     args = parser.parse_args()
 
     training_list = args.trainingfiles[0]
-    print("training files are {}".format(training_list))
 
-    print("test file is {}".format(args.testfile))
-
-    print("output file is {}".format(args.outputfile))
-
-
-    print("Starting the tagging process.")
+    # Initialize HMM class and parse training data
+    hmm = HMM()
+    parseTraining(hmm, training_list)
